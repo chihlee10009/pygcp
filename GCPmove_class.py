cce2d7ad552c27eb03718f175295b0667a10b5c9
Panda Py <chihlee10009@gmail.com>
2025-04-17 19:05:56 +0000

    added upload method to class

--- GCPmove_class.py
+++ GCPmove_class.py
@@ -7,11 +7,17 @@ from google.cloud import storage
 class GCPmove:
     def __init__(self):
         self.downloaded_file_name = "caesar_half04102025.txt"
-        storage_obj = storage.Client(project="PyScribble")
-        bucket = storage_obj.bucket("pyscribble_bucket")
-        blob = bucket.blob("caehlf04092025.txt")
+        self.storage_obj = storage.Client(project="PyScribble")
+        self.bucket = self.storage_obj.bucket("pyscribble_bucket")
+        blob = self.bucket.blob("caehlf04092025.txt")
         blob.download_to_filename(self.downloaded_file_name)
         print(f"downloaded file")
+
+    def gcp_upload(self, enc_uat):
+        self.uploaded_file_name = "hug_face_uat"
+        blob = self.bucket.blob(self.uploaded_file_name)
+        blob.upload_from_string(enc_uat)
+        print("uploaded_uat file")
 
 if __name__ == "__main__":
     GCPmove()
